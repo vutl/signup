b3827a27b49d91be0608db38922ffcec9280de32
vu tl <vutl2004@gmail.com>
2023-07-09 00:08:23 +0700

    first commit

---
+++ frontend/src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+import Login from './Login';
+import {BrowserRouter, Route, Routes } from 'react-router-dom'
+import Signup from './Signup';
+import Home from './Home';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Login /> }></Route>
+        <Route path='/signup' element={<Signup /> }></Route>
+        <Route path='/home' element={<Home /> }></Route>
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
